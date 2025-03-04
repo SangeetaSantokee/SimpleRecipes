cb660d57ed9cef3c1c96fc8d3ae54cc10e2409fd
Sangeeta Santokee <santokee.sangeeta@gmail.com>
2025-03-03 22:05:30 -0400

    Public Folder

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCd_8GDxMdjp3x7HmFZmooCp9fRNl_Dlc",
+  authDomain: "just-bytes-b0225.firebaseapp.com",
+  projectId: "just-bytes-b0225",
+  storageBucket: "just-bytes-b0225.appspot.com",
+  messagingSenderId: "831944650354",
+  appId: "1:831944650354:web:f428ccf04f931bfa51dceb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
